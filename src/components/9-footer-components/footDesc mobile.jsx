f6f192d8fc93e0a849c876d9b669d303db74b650
Gideon, I <gicodes9@gmail.com>
2023-03-05 21:51:43 +0000

    - hero release

--- src/components/9-footer-components/footDesc mobile.jsx
+++ src/components/9-footer-components/footDesc mobile.jsx
@@ -1,6 +1,4 @@
 import Grid from '@mui/material/Unstable_Grid2/Grid2';
-import Hero from '../0-hero-components/hero-kits';
-
 
 const FooterDesc = () => {
 
@@ -30,33 +28,32 @@ const FooterDesc = () => {
 
   return (
     <Grid container>
-      <Hero>
-        <Grid item>
-          <div
-            className='is-size-1 g-fonts'
-            sx={{ mobStyle }}
-            style={{
-              color: 'lemonchiffon',
-              letterSpacing: '5px',
-              textAlign: 'center',
-              fontWeight: 'bold',
-            }}>
-            Ok, Let's Connect! {handShake}
-          </div>
-          <div
-            sx={{ mobStyle }}
-            style={{ color: 'whitesmoke' }}
-            className='has-text-centered subtitle sp-fonts mt-4'
-          >
-            For more information, collaboration, or a welcome interaction, kindly reach out. I always {resPond}
-            <p className='has-text-centered mt-4'>
-              Remember, an idea that is expressed and developed, cost less than poor service.
-            </p>
-          </div>
-        </Grid>
-      </Hero >
+      <Grid item>
+        <div
+          className='is-size-1 g-fonts'
+          sx={{ mobStyle }}
+          style={{
+            color: 'lemonchiffon',
+            letterSpacing: '5px',
+            textAlign: 'center',
+            fontWeight: 'bold',
+          }}>
+          Ok, Let's Connect! {handShake}
+        </div>
+        <div
+          sx={{ mobStyle }}
+          style={{ color: 'whitesmoke' }}
+          className='has-text-centered subtitle sp-fonts mt-4'
+        >
+          For more information, collaboration, or a welcome interaction, kindly reach out. I always {resPond}
+          <p className='has-text-centered mt-4'>
+            Remember, an idea that is expressed and developed, cost less than poor service.
+          </p>
+        </div>
+      </Grid>
     </Grid>
   )
 }
+
 
 export default FooterDesc;

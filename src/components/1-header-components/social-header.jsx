e8a64b7d706ae55913945fb39dda69e7d3306aec
Gideon, I <gicodes9@gmail.com>
2023-03-09 15:23:24 +0000

    update header

--- src/components/1-header-components/social-header.jsx
+++ src/components/1-header-components/social-header.jsx
@@ -1,4 +1,56 @@
 const SocialHeader = () => {
+  const socials = (
+    <div className='level-item sp-fonts pr-5'>
+      <p className='control'>
+        <a className='is-size-5 
+          has-text-white pr-6'
+          href='https://github.com/gicodes'>
+          <span className='icon pr-3'>
+            <i className='fab fa-github'></i>
+          </span>
+          <span>
+            Github
+          </span>
+        </a>
+      </p>
+      <p className='control'>
+        <a className='is-size-5 
+          has-text-white pr-6'
+          href='https://linkedin.com/in/gideon-iduma-5311445a'>
+          <span className='icon pr-3'>
+            <i className='fab fa-linkedin'></i>
+          </span>
+          <span>
+            Linkedin
+          </span>
+        </a>
+      </p>
+      <p className='control'>
+        <a className='is-size-5 
+        has-text-white pr-6'
+          href='https://twitter.com/pappichino'>
+          <span className='icon pr-3'>
+            <i className='fab fa-twitter'></i>
+          </span>
+          <span>
+            Twitter
+          </span>
+        </a>
+      </p>
+      <p className='control'>
+        <a className='is-size-5 
+        has-text-white'
+          href='https://gicodes9@gmail.com'>
+          <span className='icon pr-3'>
+            <i className='fas fa-envelope'></i>
+          </span>
+          <span>
+            Email
+          </span>
+        </a>
+      </p>
+    </div>
+  )
 
   return (
     <nav
@@ -6,56 +58,7 @@ const SocialHeader = () => {
       style={{ backgroundColor: '#282c30' }}
       aria-label='main navigation' role='navigation'>
       <div className='navbar-end mt-6'>
-        <div className='level-item sp-fonts'>
-          <p className='control'>
-            <a className='is-size-5 
-              has-text-white pr-6'
-              href='https://github.com/gicodes'>
-              <span className='icon pr-3'>
-                <i className='fab fa-github'></i>
-              </span>
-              <span>
-                Github
-              </span>
-            </a>
-          </p>
-          <p className='control'>
-            <a className='is-size-5 
-              has-text-white pr-6'
-              href='https://linkedin.com/in/gideon-iduma-5311445a'>
-              <span className='icon pr-3'>
-                <i className='fab fa-linkedin'></i>
-              </span>
-              <span>
-                Linkedin
-              </span>
-            </a>
-          </p>
-          <p className='control'>
-            <a className='is-size-5 
-            has-text-white pr-6'
-              href='https://twitter.com/pappichino'>
-              <span className='icon pr-3'>
-                <i className='fab fa-twitter'></i>
-              </span>
-              <span>
-                Twitter
-              </span>
-            </a>
-          </p>
-          <p className='control'>
-            <a className='is-size-5 
-            has-text-white'
-              href='https://gicodes9@gmail.com'>
-              <span className='icon pr-3'>
-                <i className='fas fa-envelope'></i>
-              </span>
-              <span>
-                Email
-              </span>
-            </a>
-          </p>
-        </div>
+        {socials}
       </div>
     </nav>
   )
